73310679014e34ec98e630727cc442e3b9bb165d
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-06-30 10:15:39 +0100

    Remove zsh from homebrew

--- steps/zsh/install.sh
+++ steps/zsh/install.sh
@@ -2,10 +2,8 @@
 
 source ./script/common.sh
 
-install_brew_app zsh zsh-completions
-
 echo 'Updating shell to zsh'
-chsh -s $(brew --prefix zsh)
+chsh -s $(which zsh)
 
 echo 'Installing Oh My Zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
