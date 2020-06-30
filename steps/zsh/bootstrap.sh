9a7a4d8777f614624aaff54b65a2df05df82f390
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-06-30 13:06:08 +0100

    Check before changing shell

--- steps/zsh/bootstrap.sh
+++ steps/zsh/bootstrap.sh
@@ -2,8 +2,10 @@
 
 source ./script/common.sh
 
-echo 'Updating shell to zsh'
-chsh -s $(which zsh)
+if [[ ! $SHELL == *"/zsh" ]]; then
+    echo 'Updating shell to zsh'
+    chsh -s $(which zsh)
+fi
 
 echo 'Installing Oh My Zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
