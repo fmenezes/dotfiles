af1d693698e1229cbe9f953f1c55c18afcb0d22e
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-07-07 11:03:18 +0100

    Add uuid function

--- rc.d/functions.sh
+++ rc.d/functions.sh
@@ -5,3 +5,7 @@ function man-pdf {
 function darkmode {
   automator ~/.dotfiles/macos/darkmode.workflow 2>&1 1>/dev/null
 }
+
+function uuid {
+  python -c 'import sys,uuid; sys.stdout.write(str(uuid.uuid4()))'
+}
