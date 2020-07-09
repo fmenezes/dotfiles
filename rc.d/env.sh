ea5457aba0db49ebb504324e3fc07fe4ae874da1
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-07-09 15:09:29 +0100

    Add rbenv

--- rc.d/env.sh
+++ rc.d/env.sh
@@ -1,3 +1,8 @@
 export CODEPATH=~/Code
 export GOPATH=$CODEPATH/go
 export PATH=$GOPATH/bin:$PATH
+
+which rbenv > /dev/null
+if [ $? -eq 0 ]; then 
+  eval "$(rbenv init -)"
+fi
--- steps/brew/bootstrap.sh
+++ steps/brew/bootstrap.sh
@@ -5,4 +5,16 @@ source ./script/common.sh
 echo 'Installing Hombrew...'
 try_run 'which brew > /dev/null' 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' 'Already installed, nothing else to do'
 
-install_brew_app 'go'
+declare -a apps=(
+  'go'
+  'rbenv'
+  'rbenv-default-gems'
+)
+for app in "${apps[@]}"; do
+  install_brew_app $app
+done
+
+eval "$(rbenv init -)"
+if [ ! -f $(rbenv root)/default-gems ]; then
+  echo 'bundler' > $(rbenv root)/default-gems
+fi
