2b1ab9be3227d771018f664abaf5c2496a0ae802
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-07-10 13:04:58 +0100

    rbenv plugin

--- rc.d/env.sh
+++ rc.d/env.sh
@@ -3,8 +3,3 @@ export PATH="/usr/local/sbin:$PATH"
 export CODEPATH=~/Code
 export GOPATH=$CODEPATH/go
 export PATH=$GOPATH/bin:$PATH
-
-which rbenv > /dev/null
-if [ $? -eq 0 ]; then 
-  eval "$(rbenv init -)"
-fi
