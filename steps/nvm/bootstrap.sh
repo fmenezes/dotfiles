239d97cba9aff8240fbb800bc10a5de28eb58ad1
Filipe Menezes <filipe.menezes@Filipes-MacBook-Pro.local>
2021-01-11 08:54:24 -0300

    Fix nvm check

--- steps/nvm/bootstrap.sh
+++ steps/nvm/bootstrap.sh
@@ -2,12 +2,14 @@
 
 source ./script/common.sh
 
-function install_nvm {
-    NVM_DIR=${NVM_DIR:-~/.nvm}
+NVM_DIR=${NVM_DIR:-~/.nvm}
+
+if [ ! -d $NVM_DIR ]; then
     git clone http://github.com/nvm-sh/nvm.git "$NVM_DIR"
     cd "$NVM_DIR"
     VERSION=$(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
     git checkout -q $VERSION
-}
-
-try_run 'which nvm > /dev/null' 'install_nvm' 'Already installed, nothing else to do'
+    echo 'Done'
+else
+    echo 'Already installed, nothing else to do'
+fi
