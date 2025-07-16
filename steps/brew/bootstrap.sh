ff681cfa45fa54966ba063515a160677ef5212c2
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2025-07-16 10:49:27 +0100

    fix homebrew install

--- steps/brew/bootstrap.sh
+++ steps/brew/bootstrap.sh
@@ -3,7 +3,21 @@
 source ./script/common.sh
 
 echo 'Installing Hombrew...'
-try_run 'which brew > /dev/null' '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' 'Already installed, nothing else to do'
+
+which brew > /dev/null
+EXIT_CODE=$?
+
+if [[ "$EXIT_CODE" == "0" ]]; then
+    echo 'Already installed, nothing else to do'
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [[ "$(/usr/bin/uname -m)" == "arm64" ]]; then
+        HOMEBREW_PREFIX="/opt/homebrew"
+    else
+        HOMEBREW_PREFIX="/usr/local"
+    fi
+    eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+fi
 
 pushd .
 
